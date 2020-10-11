62a4652cb47d12794b9c864b2af63214930c70dc
bibhukiju <bibhukiju@gmail.com>
2020-10-11 15:19:26 +0545

    added a page

---
+++ flutter/AudioBook/lib/pages/playerpage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PlayPage extends StatefulWidget {
+  @override
+  _PlayPageState createState() => _PlayPageState();
+}
+
+class _PlayPageState extends State<PlayPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
